23122531ff767b42e266603f7a1b27895da805b2
Christopher Young <christopheryoung@MacBook123.local>
2023-08-08 18:11:22 -0700

    project setup

---
+++ src/data/projectsData.js
@@ -0,0 +1,172 @@
+export const projects = [
+  {
+    name: "Image Search App",
+    about: "An app that allows the user to browse for images",
+    website: "",
+    images: [
+      "https://raw.githubusercontent.com/cyoung-sudo/image-search-app-client/main/public/screenshot1.png",
+      "https://raw.githubusercontent.com/cyoung-sudo/image-search-app-client/main/public/screenshot2.png"
+    ],
+    tech: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "Node",
+      "Express"
+    ],
+    repos: {
+      client: "https://github.com/cyoung-sudo/image-search-app-client",
+      server: "https://github.com/cyoung-sudo/image-search-app-server"
+    },
+    deployed: false
+  },
+
+  {
+    name: "Voting App",
+    about: "An app that allows the user to create and vote on polls",
+    website: "",
+    images: [
+      "https://raw.githubusercontent.com/cyoung-sudo/voting-app-client/main/public/screenshot1.png",
+      "https://raw.githubusercontent.com/cyoung-sudo/voting-app-client/main/public/screenshot2.png",
+      "https://raw.githubusercontent.com/cyoung-sudo/voting-app-client/main/public/screenshot3.png"
+    ],
+    tech: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "Node",
+      "Express",
+      "MongoDB Atlas"
+    ],
+    repos: {
+      client: "https://github.com/cyoung-sudo/voting-app-client",
+      server: "https://github.com/cyoung-sudo/voting-app-server"
+    },
+    deployed: false
+  },
+
+  {
+    name: "Nightlife Coordination App",
+    about: "An app that allows the user to browse and track places they want to attend",
+    website: "",
+    images: [
+      "https://raw.githubusercontent.com/cyoung-sudo/nightlife-coordination-app-client/main/public/screenshot1.png",
+      "https://raw.githubusercontent.com/cyoung-sudo/nightlife-coordination-app-client/main/public/screenshot2.png",
+      "https://raw.githubusercontent.com/cyoung-sudo/nightlife-coordination-app-client/main/public/screenshot3.png"
+    ],
+    tech: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "Node",
+      "Express",
+      "MongoDB Atlas"
+    ],
+    repos: {
+      client: "https://github.com/cyoung-sudo/nightlife-coordination-app-client",
+      server: "https://github.com/cyoung-sudo/nightlife-coordination-app-server"
+    },
+    deployed: false
+  },
+
+  {
+    name: "Stock Market App",
+    about: "An app that allows the user to compare the weekly price-action of 2 stocks",
+    website: "",
+    images: [
+      "https://raw.githubusercontent.com/cyoung-sudo/stock-market-app-client/main/public/screenshot1.png",
+      "https://raw.githubusercontent.com/cyoung-sudo/stock-market-app-client/main/public/screenshot2.png"
+    ],
+    tech: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "Node",
+      "Express",
+      "MongoDB Atlas"
+    ],
+    repos: {
+      client: "https://github.com/cyoung-sudo/stock-market-app-client",
+      server: "https://github.com/cyoung-sudo/stock-market-app-server"
+    },
+    deployed: false
+  },
+
+  {
+    name: "Book Trading App",
+    about: "An app that allows the user to upload and trade books with other users",
+    website: "",
+    images: [
+      "https://raw.githubusercontent.com/cyoung-sudo/book-trading-app-client/main/public/screenshot1.png",
+      "https://raw.githubusercontent.com/cyoung-sudo/book-trading-app-client/main/public/screenshot2.png",
+      "https://raw.githubusercontent.com/cyoung-sudo/book-trading-app-client/main/public/screenshot3.png"
+    ],
+    tech: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "Node",
+      "Express",
+      "MongoDB Atlas"
+    ],
+    repos: {
+      client: "https://github.com/cyoung-sudo/book-trading-app-client",
+      server: "https://github.com/cyoung-sudo/book-trading-app-server"
+    },
+    deployed: false
+  },
+
+  {
+    name: "Pinterest Clone App",
+    about: "An app that allows the user to upload and share images with other users",
+    website: "",
+    images: [
+      "https://raw.githubusercontent.com/cyoung-sudo/pinterest-clone-app-client/main/public/screenshot1.png",
+      "https://raw.githubusercontent.com/cyoung-sudo/pinterest-clone-app-client/main/public/screenshot2.png"
+    ],
+    tech: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "Node",
+      "Express",
+      "MongoDB Atlas"
+    ],
+    repos: {
+      client: "https://github.com/cyoung-sudo/pinterest-clone-app-client",
+      server: "https://github.com/cyoung-sudo/pinterest-clone-app-server"
+    },
+    deployed: false
+  },
+
+  {
+    name: "Blog App",
+    about: "An app that allows the user to create and share posts with other users",
+    website: "http://34.227.172.138/",
+    images: [
+      "https://raw.githubusercontent.com/cyoung-sudo/blog-app-client/main/public/screenshot1.png",
+      "https://raw.githubusercontent.com/cyoung-sudo/blog-app-client/main/public/screenshot2.png",
+      "https://raw.githubusercontent.com/cyoung-sudo/blog-app-client/main/public/screenshot3.png"
+    ],
+    tech: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "Node",
+      "Express",
+      "MongoDB Atlas"
+    ],
+    repos: {
+      client: "https://github.com/cyoung-sudo/blog-app-client",
+      server: "https://github.com/cyoung-sudo/blog-app-server"
+    },
+    deployed: true
+  }
+];
